e1b570cdc423b813637437ac420e0e36778e9007
Pedro Santos <pedro.san7os@gmail.com>
2014-11-05 15:47:28 +0000

    Code extracted from fenix

---
+++ src/main/java/net/sourceforge/fenixedu/presentationTier/Action/departmentAdmOffice/DepartmentAdmOfficeExamsApp.java
@@ -0,0 +1,9 @@
+package org.fenixedu.academic.ui.struts.action.departmentAdmOffice;
+
+import org.fenixedu.bennu.portal.StrutsApplication;
+
+@StrutsApplication(bundle = "VigilancyResources", path = "exam-coordination",
+        titleKey = "label.navheader.person.examCoordinator", hint = "Department Admin Office",
+        accessGroup = "role(DEPARTMENT_ADMINISTRATIVE_OFFICE)")
+public class DepartmentAdmOfficeExamsApp {
+}
