c8b89afdbad774b1ff20202775eb947241c37873
Pedro Santos <pedro.san7os@gmail.com>
2014-11-20 11:06:30 +0000

    Included missing apps from fenix extraction

---
+++ src/main/java/net/sourceforge/fenixedu/presentationTier/Action/pedagogicalCouncil/TutorshipApp.java
@@ -0,0 +1,8 @@
+package net.sourceforge.fenixedu.presentationTier.Action.pedagogicalCouncil;
+
+import org.fenixedu.bennu.struts.portal.StrutsApplication;
+
+@StrutsApplication(bundle = "PedagogicalCouncilResources", path = "tutorship", titleKey = "link.tutorship", accessGroup = "role(TUTORSHIP)",
+        hint = "Pedagogical Council")
+public class TutorshipApp {
+}
