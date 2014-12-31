d7d7d62947ab2ac41c440b06809cbae67401818d
sergiofbsilva <sergiofbsilva@gmail.com>
2014-12-31 15:46:39 +0000

    Remove unnecessary actions

--- src/main/java/net/sourceforge/fenixedu/presentationTier/Action/pedagogicalCouncil/TutorshipApp.java
+++
@@ -1,8 +0,0 @@
-package net.sourceforge.fenixedu.presentationTier.Action.pedagogicalCouncil;
-
-import org.fenixedu.bennu.struts.portal.StrutsApplication;
-
-@StrutsApplication(bundle = "PedagogicalCouncilResources", path = "tutorship", titleKey = "link.tutorship", accessGroup = "role(TUTORSHIP)",
-        hint = "Pedagogical Council")
-public class TutorshipApp {
-}
--- src/main/java/net/sourceforge/fenixedu/presentationTier/Action/teacher/tutor/TeacherTutorApp.java
+++
@@ -1,8 +0,0 @@
-package net.sourceforge.fenixedu.presentationTier.Action.teacher.tutor;
-
-import org.fenixedu.bennu.struts.portal.StrutsApplication;
-
-@StrutsApplication(bundle = "ApplicationResources", path = "tutor", titleKey = "link.teacher.tutor.operations",
-        accessGroup = "role(TEACHER) | professors", hint = "Teacher")
-public class TeacherTutorApp {
-}
