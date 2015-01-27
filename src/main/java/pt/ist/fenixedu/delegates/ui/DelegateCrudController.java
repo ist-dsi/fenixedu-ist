fc3c0d7d88174afd127f30e14f5a5835feb8e6e5
Pedro Santos <pedro.san7os@gmail.com>
2015-01-27 17:12:56 +0000

    Updated license headers

--- src/main/java/pt/ist/fenixedu/delegates/ui/DelegateCrudController.java
+++ src/main/java/pt/ist/fenixedu/delegates/ui/DelegateCrudController.java
@@ -1,20 +1,20 @@
 /**
- * Copyright © 2014 Instituto Superior Técnico
+ * Copyright © 2011 Instituto Superior Técnico
  *
- * This file is part of FenixEdu Spaces.
+ * This file is part of FenixEdu Delegates.
  *
- * FenixEdu Spaces is free software: you can redistribute it and/or modify
+ * FenixEdu Delegates is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * FenixEdu Spaces is distributed in the hope that it will be useful,
+ * FenixEdu Delegates is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with FenixEdu Spaces.  If not, see <http://www.gnu.org/licenses/>.
+ * along with FenixEdu Delegates.  If not, see <http://www.gnu.org/licenses/>.
  */
 package pt.ist.fenixedu.delegates.ui;
 
