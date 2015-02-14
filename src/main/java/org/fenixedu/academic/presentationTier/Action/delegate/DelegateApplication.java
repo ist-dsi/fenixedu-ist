97708e48aae9fc4658df54b87d3c6df60e296b1d
Pedro Santos <pedro.san7os@gmail.com>
2015-02-14 00:00:34 +0000

    Removed delegate related funcionalities

--- src/main/java/org/fenixedu/academic/presentationTier/Action/delegate/DelegateApplication.java
+++ src/main/java/org/fenixedu/academic/presentationTier/Action/delegate/DelegateApplication.java
@@ -18,59 +18,12 @@
  */
 package org.fenixedu.academic.presentationTier.Action.delegate;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
-import org.fenixedu.bennu.core.domain.User;
-import org.fenixedu.bennu.core.security.Authenticate;
-import org.fenixedu.bennu.struts.annotations.Mapping;
 import org.fenixedu.bennu.struts.portal.StrutsApplication;
-import org.fenixedu.bennu.struts.portal.StrutsFunctionality;
-
-import pt.ist.fenixedu.delegates.domain.student.Delegate;
 
 public class DelegateApplication {
-
-    @StrutsApplication(bundle = "DelegateResources", path = "consult", titleKey = "label.delegates.consult",
-            accessGroup = "delegate", hint = "Delegate")
-    public static class DelegateConsultApp {
-
-    }
-
-    @StrutsApplication(bundle = "DelegateResources", path = "communication", titleKey = "label.delegates.comunication",
-            accessGroup = "delegate", hint = "Delegate")
-    public static class DelegateMessagingApp {
-
-    }
 
     @StrutsApplication(bundle = "DelegateResources", path = "participate", titleKey = "label.participate",
             accessGroup = "delegate", hint = "Delegate")
     public static class DelegateParticipateApp {
-
     }
-
-    // TODO Fix the access group!
-    @StrutsFunctionality(app = DelegateConsultApp.class, path = "evaluations", titleKey = "link.evaluations",
-            accessGroup = "anyone")
-    @Mapping(path = "/evaluationsForDelegates", module = "delegate")
-    public static class EvaluationsForDelegatesAction extends Action {
-
-        @Override
-        public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
-                HttpServletResponse response) throws Exception {
-            User user = Authenticate.getUser();
-            if (user != null) {
-                Delegate function = user.getDelegatesSet().stream().filter(d -> d.isActive()).findFirst().get();
-                if (function != null) {
-                    return new ActionForward("/evaluationsForDelegates.faces?degreeID=" + function.getDegree().getExternalId());
-                }
-            }
-            return null;
-        }
-    }
-
 }
