ec95664be15317b0bf69aa4c6c5aa49155d0b9b3
João Carvalho <joao.pedro.carvalho@tecnico.ulisboa.pt>
2015-04-21 15:26:21 +0100

    Disconnect QuestionAnswer from Bennu

--- fenixedu-ist-quc/src/main/java/pt/ist/fenixedu/quc/domain/QuestionAnswer.java
+++ fenixedu-ist-quc/src/main/java/pt/ist/fenixedu/quc/domain/QuestionAnswer.java
@@ -18,13 +18,11 @@
  */
 package pt.ist.fenixedu.quc.domain;
 
-import org.fenixedu.bennu.core.domain.Bennu;
 
 public class QuestionAnswer extends QuestionAnswer_Base {
 
     public QuestionAnswer(InquiryAnswer inquiryAnswer, InquiryQuestion inquiryQuestion, String value) {
         super();
-        setRootDomainObject(Bennu.getInstance());
         setInquiryAnswer(inquiryAnswer);
         setInquiryQuestion(inquiryQuestion);
         setAnswer(value);
@@ -33,7 +31,6 @@ public class QuestionAnswer extends QuestionAnswer_Base {
     public void delete() {
         setInquiryQuestion(null);
         setInquiryAnswer(null);
-        setRootDomainObject(null);
         super.deleteDomainObject();
     }
 
