b64e7631039e12e3c61a65e144e10bd0944da1e8
João Carvalho <joaopecarvalho@gmail.com>
2016-01-29 10:18:58 +0000

    Removed unused Pre-Bolonha Master Degree Thesis
      * Issue: FIST-225

--- fenixedu-ist-pre-bolonha/src/main/java/org/fenixedu/academic/ui/struts/action/masterDegree/administrativeOffice/MasterDegreeOfficeApplication.java
+++ fenixedu-ist-pre-bolonha/src/main/java/org/fenixedu/academic/ui/struts/action/masterDegree/administrativeOffice/MasterDegreeOfficeApplication.java
@@ -22,14 +22,9 @@ import org.fenixedu.bennu.struts.portal.StrutsApplication;
 
 public class MasterDegreeOfficeApplication {
 
-    @StrutsApplication(bundle = "ApplicationResources", path = "master-degree-candidates", titleKey = "title.candidate.main",
+    @StrutsApplication(
+            bundle = "AdminOffice", path = "dfa", titleKey = "link.masterDegree.administrativeOffice.dfaCandidacyManagement",
             hint = "Master Degree Office", accessGroup = "role(MASTER_DEGREE_ADMINISTRATIVE_OFFICE)")
-    public static class MasterDegreeCandidatesApp {
-    }
-
-    @StrutsApplication(bundle = "AdminOffice", path = "dfa",
-            titleKey = "link.masterDegree.administrativeOffice.dfaCandidacyManagement", hint = "Master Degree Office",
-            accessGroup = "role(MASTER_DEGREE_ADMINISTRATIVE_OFFICE)")
     public static class MasterDegreeDfaApp {
     }
 
