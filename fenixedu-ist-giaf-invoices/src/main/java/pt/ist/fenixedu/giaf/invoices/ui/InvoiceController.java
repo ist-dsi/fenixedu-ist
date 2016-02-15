97af20fa903fecfd4ebe877578a656a74a65a9bf
Luis Cruz <luis.cruz@tecnico.pt>
2016-02-15 17:39:30 +0000

    Make invoice interface available to academic services. Add information to allow services to diagnose problems with the production of invoices.

--- fenixedu-ist-giaf-invoices/src/main/java/pt/ist/fenixedu/giaf/invoices/ui/InvoiceController.java
+++ fenixedu-ist-giaf-invoices/src/main/java/pt/ist/fenixedu/giaf/invoices/ui/InvoiceController.java
@@ -18,11 +18,12 @@
  */
 package pt.ist.fenixedu.giaf.invoices.ui;
 
-import java.util.Collections;
 import java.util.stream.Collector;
 import java.util.stream.Collector.Characteristics;
 
 import org.fenixedu.academic.domain.Person;
+import org.fenixedu.academic.domain.accessControl.AcademicAuthorizationGroup;
+import org.fenixedu.academic.domain.accessControl.academicAdministration.AcademicOperationType;
 import org.fenixedu.academic.domain.accounting.Event;
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.core.security.Authenticate;
@@ -30,6 +31,7 @@ import org.fenixedu.bennu.spring.portal.SpringApplication;
 import org.fenixedu.bennu.spring.portal.SpringFunctionality;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -44,15 +46,32 @@ import pt.ist.fenixedu.giaf.invoices.GiafEvent;
 public class InvoiceController {
 
     @RequestMapping
-    public String home(final Model model) {
-        final User user = Authenticate.getUser();
-        final Person person = user.getPerson();
+    public String home(@RequestParam(required = false) String username, final Model model) {
+        final User user = getUser(username);
 
-        final JsonArray events = new JsonArray();
-        person.getEventsSet().stream().map(this::toJsonArray).forEach(a -> events.addAll(a));
-        model.addAttribute("events", events);
+        if (isAllowedToAccess(user)) {
+            final Person person = user.getPerson();
+            
+            final JsonArray events = new JsonArray();
+            person.getEventsSet().stream().sorted(Event.COMPARATOR_BY_DATE).map(this::toJsonArray).forEach(a -> events.addAll(a));
+            model.addAttribute("events", events);
+        }
 
         return "giaf-invoice-viewer/home";
+    }
+
+    private boolean isAllowedToAccess(final User user) {
+        final User currentUser = Authenticate.getUser();
+        return currentUser == user || isAcademicServiceStaff(currentUser);
+    }
+
+    private boolean isAcademicServiceStaff(final User user) {
+        return AcademicAuthorizationGroup.get(AcademicOperationType.MANAGE_STUDENT_PAYMENTS).isMember(user)
+                || AcademicAuthorizationGroup.get(AcademicOperationType.MANAGE_STUDENT_PAYMENTS_ADV).isMember(user);
+    }
+
+    private User getUser(final String username) {
+        return username == null || username.isEmpty() ? Authenticate.getUser() : User.findByUsername(username);
     }
 
     private JsonArray toJsonArray(final Event event) {
