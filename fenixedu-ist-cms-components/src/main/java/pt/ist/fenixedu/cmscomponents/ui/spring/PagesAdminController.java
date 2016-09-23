553324b536e0e8e4ad65f08115f0e949cd37f511
diutsu <sousago@gmail.com>
2016-09-23 12:16:45 +0100

    permission checks on techer's site interface FIST-348 #resolve

--- fenixedu-ist-cms-components/src/main/java/pt/ist/fenixedu/cmscomponents/ui/spring/PagesAdminController.java
+++ fenixedu-ist-cms-components/src/main/java/pt/ist/fenixedu/cmscomponents/ui/spring/PagesAdminController.java
@@ -30,6 +30,7 @@ import org.fenixedu.bennu.core.security.Authenticate;
 import org.fenixedu.bennu.io.domain.GroupBasedFile;
 import org.fenixedu.cms.domain.MenuItem;
 import org.fenixedu.cms.domain.PermissionEvaluation;
+import org.fenixedu.cms.domain.PermissionsArray.Permission;
 import org.fenixedu.cms.domain.Site;
 import org.fenixedu.cms.exceptions.CmsDomainException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,7 +48,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import pt.ist.fenixframework.FenixFramework;
-
+import static org.fenixedu.cms.domain.PermissionEvaluation.ensureCanDoThis;
 @RestController
 @RequestMapping("/pages/{siteId}/admin")
 public class PagesAdminController {
@@ -59,11 +60,14 @@ public class PagesAdminController {
 
     @RequestMapping(value = "/data", method = RequestMethod.GET, produces = JSON_VALUE)
     public @ResponseBody String data(@PathVariable String siteId) {
+        ensureCanDoThis(site(siteId), Permission.SEE_PAGES, Permission.LIST_MENUS);
         return service.serialize(site(siteId)).toString();
     }
 
+
     @RequestMapping(value = "/data/{menuItem}", method = RequestMethod.GET, produces = JSON_VALUE)
     public @ResponseBody String data(@PathVariable String siteId, @PathVariable MenuItem menuItem) {
+        ensureCanDoThis(site(siteId), Permission.LIST_MENUS,Permission.SEE_PAGES);
         return service.data(site(siteId), menuItem).toString();
     }
 
@@ -71,19 +75,32 @@ public class PagesAdminController {
     public @ResponseBody String create(@PathVariable String siteId, @RequestBody String bodyJson) {
         PagesAdminBean bean = new PagesAdminBean(bodyJson);
         Site site = site(siteId);
+        ensureCanDoThis(site, Permission.CREATE_MENU_ITEM, Permission.CREATE_POST, Permission.CREATE_PAGE);
+        if (bean.getParent() != null && bean.getParent().getMenu().getPrivileged()) {
+            ensureCanDoThis(site, Permission.EDIT_PRIVILEGED_MENU);
+        }
         Optional<MenuItem> menuItem = service.create(site, bean.getParent(), bean.getTitle(), bean.getBody(),bean.getExcerpt());
         return service.serialize(menuItem.get(), true).toString();
     }
 
     @RequestMapping(value = "/{menuItemId}", method = RequestMethod.DELETE)
     public @ResponseBody String delete(@PathVariable String siteId, @PathVariable String menuItemId) {
-        service.delete(getDomainObject(menuItemId));
+        MenuItem item = FenixFramework.getDomainObject(menuItemId);
+        ensureCanDoThis(item.getMenu().getSite(), Permission.DELETE_MENU, Permission.EDIT_MENU);
+        if (item.getMenu().getPrivileged()) {
+            ensureCanDoThis(item.getMenu().getSite(), Permission.DELETE_PRIVILEGED_MENU, Permission.EDIT_PRIVILEGED_MENU);
+        }
+        service.delete(item);
         return data(siteId);
     }
 
     @RequestMapping(method = RequestMethod.PUT, consumes = JSON_VALUE)
     public @ResponseBody String edit(@RequestBody String bodyJson) {
         PagesAdminBean bean = new PagesAdminBean(bodyJson);
+        ensureCanDoThis(bean.getMenuItem().getMenu().getSite(), Permission.LIST_MENUS, Permission.EDIT_MENU, Permission.EDIT_PAGE, Permission.EDIT_POSTS);
+        if (bean.getMenuItem().getMenu().getPrivileged()) {
+            ensureCanDoThis(bean.getMenuItem().getMenu().getSite(), Permission.DELETE_PRIVILEGED_MENU, Permission.EDIT_PRIVILEGED_MENU);
+        }
         MenuItem menuItem =
                 service.edit(bean.getMenuItem(), bean.getTitle(), bean.getBody(), bean.getExcerpt(), bean.getCanViewGroup(), bean.isVisible());
         return service.serialize(menuItem, true).toString();
@@ -92,9 +109,10 @@ public class PagesAdminController {
     @RequestMapping(value = "{menuItemId}/addFile.json", method = RequestMethod.POST)
     public @ResponseBody String addFileJson(@PathVariable("menuItemId") String menuItemId,
             @RequestParam("file") MultipartFile file) throws IOException {
-        MenuItem menuItem = FenixFramework.getDomainObject(menuItemId);
-        GroupBasedFile addedFile = service.addPostFile(file, menuItem);
-        return service.describeFile(menuItem.getPage(), addedFile).toString();
+        MenuItem item = FenixFramework.getDomainObject(menuItemId);
+        GroupBasedFile addedFile = service.addPostFile(file, item);
+        ensureCanDoThis(item.getMenu().getSite(), Permission.LIST_MENUS, Permission.EDIT_POSTS, Permission.EDIT_PAGE);
+        return service.describeFile(item.getPage(), addedFile).toString();
     }
 
     @RequestMapping(value = "/move", method = RequestMethod.PUT, consumes = JSON_VALUE)
@@ -104,6 +122,12 @@ public class PagesAdminController {
         MenuItem parent = getDomainObject(json.get("parent").getAsString());
         MenuItem insertAfter =
                 getDomainObject(json.get("insertAfter").isJsonNull() ? null : json.get("insertAfter").getAsString());
+        ensureCanDoThis(item.getMenu().getSite(), Permission.LIST_MENUS, Permission.EDIT_MENU, Permission.EDIT_MENU_ITEM);
+        if (item.getMenu().getPrivileged()
+          || (insertAfter!=null && insertAfter.getMenu().getPrivileged())
+          || (parent !=null && parent.getMenu().getPrivileged())) {
+            ensureCanDoThis(item.getMenu().getSite(), Permission.DELETE_PRIVILEGED_MENU, Permission.EDIT_PRIVILEGED_MENU);
+        }
         service.moveTo(item, parent, insertAfter);
         return service.serialize(item, false).toString();
     }
@@ -111,7 +135,9 @@ public class PagesAdminController {
     @RequestMapping(value = "/attachment/{menuItemId}", method = RequestMethod.POST)
     public @ResponseBody String addAttachments(@PathVariable("menuItemId") String menuItemId,
             @RequestParam("file") MultipartFile file) throws IOException {
-        service.addAttachment(file.getOriginalFilename(), file, getDomainObject(menuItemId));
+        MenuItem item = getDomainObject(menuItemId);
+        ensureCanDoThis(item.getMenu().getSite(),Permission.LIST_MENUS, Permission.EDIT_POSTS, Permission.EDIT_PAGE);
+        service.addAttachment(file.getOriginalFilename(), file, item);
         return getAttachments(menuItemId);
     }
 
@@ -119,6 +145,7 @@ public class PagesAdminController {
     public @ResponseBody String deleteAttachments(@PathVariable String menuItemId, @PathVariable String fileId) {
         MenuItem menuItem = getDomainObject(menuItemId);
         GroupBasedFile postFile = getDomainObject(fileId);
+        ensureCanDoThis(menuItem.getMenu().getSite(),Permission.LIST_MENUS, Permission.EDIT_POSTS, Permission.EDIT_PAGE);
         service.delete(menuItem, postFile);
         return getAttachments(menuItemId);
     }
@@ -126,6 +153,7 @@ public class PagesAdminController {
     @RequestMapping(value = "/attachments", method = RequestMethod.GET)
     public @ResponseBody String getAttachments(@RequestParam(required = true) String menuItemId) {
         MenuItem menuItem = getDomainObject(menuItemId);
+        ensureCanDoThis(menuItem.getMenu().getSite(),Permission.LIST_MENUS, Permission.SEE_PAGES);
         return service.serializeAttachments(menuItem.getPage()).toString();
     }
 
@@ -134,9 +162,10 @@ public class PagesAdminController {
         JsonObject updateMessage = new JsonParser().parse(bodyJson).getAsJsonObject();
         MenuItem menuItem = getDomainObject(updateMessage.get("menuItemId").getAsString());
         GroupBasedFile attachment = getDomainObject(updateMessage.get("fileId").getAsString());
+        ensureCanDoThis(menuItem.getMenu().getSite(),Permission.LIST_MENUS, Permission.EDIT_POSTS, Permission.EDIT_PAGE);
         service.updateAttachment(menuItem, attachment, updateMessage.get("position").getAsInt(),
-                updateMessage.get("group").getAsInt(), updateMessage.get("name").getAsString(),
-                updateMessage.get("visible").getAsBoolean());
+            updateMessage.get("group").getAsInt(), updateMessage.get("name").getAsString(),
+            updateMessage.get("visible").getAsBoolean());
         return getAttachments(menuItem.getExternalId());
     }
 
