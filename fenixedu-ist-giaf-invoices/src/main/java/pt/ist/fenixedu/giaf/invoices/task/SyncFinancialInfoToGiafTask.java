9511536a35ceab90dbd8f7a253e26d6921d0cb3b
Luis Cruz <luis.cruz@tecnico.pt>
2017-08-28 12:52:16 +0100

    New automated script to report third party receipts. Also implemented lock mecanism to avoid multiple running threads in parallel. (#348)

--- fenixedu-ist-giaf-invoices/src/main/java/pt/ist/fenixedu/giaf/invoices/task/SyncFinancialInfoToGiafTask.java
+++ fenixedu-ist-giaf-invoices/src/main/java/pt/ist/fenixedu/giaf/invoices/task/SyncFinancialInfoToGiafTask.java
@@ -1,32 +1,16 @@
 package pt.ist.fenixedu.giaf.invoices.task;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.HashSet;
-import java.util.Properties;
 import java.util.Set;
 import java.util.stream.Stream;
 
-import javax.activation.DataHandler;
-import javax.activation.DataSource;
-import javax.mail.Message;
 import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.Session;
-import javax.mail.Transport;
 import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-import javax.mail.util.ByteArrayDataSource;
 
-import org.fenixedu.academic.FenixEduAcademicConfiguration;
 import org.fenixedu.academic.domain.Person;
 import org.fenixedu.academic.domain.accounting.Event;
-import org.fenixedu.academic.domain.util.email.Sender;
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.bennu.scheduler.CronTask;
 import org.fenixedu.bennu.scheduler.annotation.Task;
@@ -39,15 +23,19 @@ import pt.ist.fenixedu.giaf.invoices.ErrorLogConsumer;
 import pt.ist.fenixedu.giaf.invoices.EventLogger;
 import pt.ist.fenixedu.giaf.invoices.EventProcessor;
 import pt.ist.fenixedu.giaf.invoices.EventWrapper;
+import pt.ist.fenixedu.giaf.invoices.task.TaskUtils.LockManager;
 
 @Task(englishTitle = "Sync financial information (debts, reciepts, exemptions and clients) with GIAF.")
 public class SyncFinancialInfoToGiafTask extends CronTask {
 
-    private static final String EMAIL_ADDRESSES_TO_SEND_DATA_FILENAME  = "/afs/ist.utl.pt/ciist/fenix/fenix015/ist/giaf_sync_errors_to.txt";
-    private static final String EMAIL_ADDRESSES_BCC_SEND_DATA_FILENAME = "/afs/ist.utl.pt/ciist/fenix/fenix015/ist/giaf_sync_errors_bcc.txt";
-
     @Override
     public void runTask() throws Exception {
+        try (final LockManager locker = TaskUtils.getLockManager()) {
+            runSyncScript();
+        }
+    }
+
+    private void runSyncScript() throws IOException, AddressException, MessagingException {
         touch("Start");
 
         final Spreadsheet errors = new Spreadsheet("Errors");
@@ -125,7 +113,9 @@ public class SyncFinancialInfoToGiafTask extends CronTask {
         touch("Dumping error messages.");
         final ByteArrayOutputStream stream = new ByteArrayOutputStream();
         errors.exportToXLSSheet(stream);
-        sendErrorReport("errors" + new DateTime().toString("yyyy_MM_dd_HH_mm") + ".xls", stream.toByteArray());
+        final String subject = "Problemas no envio de informação para o GIAF";
+        final String body = "Listagem atualizada com os problemas verificados na sincronização de informação financeira entre o Fénix e o GIAF: " + new DateTime().toString("yyyy-MM-dd HH:mm");
+        TaskUtils.sendReport("errors" + new DateTime().toString("yyyy_MM_dd_HH_mm") + ".xls", stream.toByteArray(), subject, body);
 
         touch("Done");
     }
@@ -137,46 +127,6 @@ public class SyncFinancialInfoToGiafTask extends CronTask {
     private Stream<Event> unfilteredEventStream(final ErrorLogConsumer consumer) {
         return EventWrapper.eventsToProcess(consumer, Bennu.getInstance().getAccountingEventsSet().stream(),
                 Bennu.getInstance().getAccountingTransactionDetailsSet().stream());
-    }
-
-    private void sendErrorReport(String filename, byte[] byteArray) throws AddressException, MessagingException {
-        final Properties properties = new Properties();
-        properties.put("mail.smtp.host", FenixEduAcademicConfiguration.getConfiguration().getMailSmtpHost());
-        properties.put("mail.smtp.name", FenixEduAcademicConfiguration.getConfiguration().getMailSmtpName());
-        properties.put("mailSender.max.recipients", FenixEduAcademicConfiguration.getConfiguration().getMailSenderMaxRecipients());
-        properties.put("mail.debug", "false");
-        final Session session = Session.getDefaultInstance(properties, null);
-
-        final Sender sender = Bennu.getInstance().getSystemSender();
-
-        final Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress(sender.getFromAddress()));
-        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(fileContent(EMAIL_ADDRESSES_TO_SEND_DATA_FILENAME)));
-        message.setRecipients(Message.RecipientType.BCC, InternetAddress.parse(fileContent(EMAIL_ADDRESSES_BCC_SEND_DATA_FILENAME)));
-        message.setSubject("Problemas no envio de informação para o GIAF");
-        message.setText("Listagem atualizada com os problemas verificados na sincronização de informação financeira entre o Fénix e o GIAF: " + new DateTime().toString("yyyy-MM-dd HH:mm"));
-
-        MimeBodyPart messageBodyPart = new MimeBodyPart();
-
-        Multipart multipart = new MimeMultipart();
-
-        messageBodyPart = new MimeBodyPart();
-        DataSource source = new ByteArrayDataSource(byteArray, "application/vnd.ms-excel");
-        messageBodyPart.setDataHandler(new DataHandler(source));
-        messageBodyPart.setFileName(filename);
-        multipart.addBodyPart(messageBodyPart);
-
-        message.setContent(multipart);
-
-        Transport.send(message);
-    }
-
-    private String fileContent(final String filename) {
-        try {
-            return Files.readAllLines(new File(filename).toPath()).iterator().next();
-        } catch (final IOException e) {
-            throw new Error(e);
-        }
     }
 
 }
---
+++ fenixedu-ist-giaf-invoices/src/main/java/pt/ist/fenixedu/giaf/invoices/task/TaskUtils.java
@@ -0,0 +1,120 @@
+package pt.ist.fenixedu.giaf.invoices.task;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.util.ByteArrayDataSource;
+
+import org.fenixedu.academic.FenixEduAcademicConfiguration;
+import org.fenixedu.academic.domain.util.email.Sender;
+import org.fenixedu.academic.util.ConnectionManager;
+import org.fenixedu.bennu.core.domain.Bennu;
+
+public class TaskUtils {
+
+    static final String EMAIL_ADDRESSES_TO_SEND_DATA_FILENAME  = "/afs/ist.utl.pt/ciist/fenix/fenix015/ist/giaf_sync_errors_to.txt";
+    static final String EMAIL_ADDRESSES_BCC_SEND_DATA_FILENAME = "/afs/ist.utl.pt/ciist/fenix/fenix015/ist/giaf_sync_errors_bcc.txt";
+    static final String LOCK_VARIABLE = "GIAF_INVOICE_SYNC_VAR";
+
+    static void sendReport(final String filename, final byte[] byteArray, final String subject, final String body) throws AddressException, MessagingException {
+        final Properties properties = new Properties();
+        properties.put("mail.smtp.host", FenixEduAcademicConfiguration.getConfiguration().getMailSmtpHost());
+        properties.put("mail.smtp.name", FenixEduAcademicConfiguration.getConfiguration().getMailSmtpName());
+        properties.put("mailSender.max.recipients", FenixEduAcademicConfiguration.getConfiguration().getMailSenderMaxRecipients());
+        properties.put("mail.debug", "false");
+        final Session session = Session.getDefaultInstance(properties, null);
+
+        final Sender sender = Bennu.getInstance().getSystemSender();
+
+        final Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress(sender.getFromAddress()));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(fileContent(EMAIL_ADDRESSES_TO_SEND_DATA_FILENAME)));
+        message.setRecipients(Message.RecipientType.BCC, InternetAddress.parse(fileContent(EMAIL_ADDRESSES_BCC_SEND_DATA_FILENAME)));
+        message.setSubject(subject);
+        message.setText(body);
+
+        MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+        Multipart multipart = new MimeMultipart();
+
+        messageBodyPart = new MimeBodyPart();
+        DataSource source = new ByteArrayDataSource(byteArray, "application/vnd.ms-excel");
+        messageBodyPart.setDataHandler(new DataHandler(source));
+        messageBodyPart.setFileName(filename);
+        multipart.addBodyPart(messageBodyPart);
+
+        message.setContent(multipart);
+
+        Transport.send(message);
+    }
+
+    private static String fileContent(final String filename) {
+        try {
+            return Files.readAllLines(new File(filename).toPath()).iterator().next();
+        } catch (final IOException e) {
+            throw new Error(e);
+        }
+    }
+
+    public static class LockManager implements AutoCloseable {
+
+        Connection connection = ConnectionManager.getCurrentSQLConnection();
+
+        private LockManager() {
+            try {
+                final PreparedStatement statement = connection.prepareStatement(
+                        "select GET_LOCK('" + LOCK_VARIABLE + "', 5);");
+                final ResultSet resultSet = statement.executeQuery();
+                if (resultSet.next() && resultSet.getInt(1) == 1) {
+                    System.out.println("Lock obtained for " + LOCK_VARIABLE + ". Running...");
+                } else {
+                    releaseLock();
+                    throw new Error("Unable to obtain lock for " + LOCK_VARIABLE);
+                }
+            } catch (final SQLException e) {
+                throw new Error(e);
+            }
+        }
+
+        private void releaseLock() {
+            try {
+                final PreparedStatement statement = connection.prepareStatement(
+                        "select RELEASE_LOCK('" + LOCK_VARIABLE + "');");
+                statement.executeQuery();
+                System.out.println("Released lock for " + LOCK_VARIABLE);
+            } catch(final Throwable t) {
+                System.out.println("Unable to released lock for" + LOCK_VARIABLE);
+            }
+        }
+
+        @Override
+        public void close() throws Exception {
+            releaseLock();
+            connection = null;
+        }
+        
+    }
+
+    static LockManager getLockManager() {
+        return new LockManager();
+    }
+    
+}
