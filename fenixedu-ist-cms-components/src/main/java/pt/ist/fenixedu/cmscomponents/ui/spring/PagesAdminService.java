5e074bdd1b8f42f955a35c96e40b8a66afcb3421
Daniel Correia <dduarte96@hotmail.com>
2017-12-19 14:42:27 +0000

    Changing permission of Page item does not update its children Page items FIST-515 #resolve

--- fenixedu-ist-cms-components/src/main/java/pt/ist/fenixedu/cmscomponents/ui/spring/PagesAdminService.java
+++ fenixedu-ist-cms-components/src/main/java/pt/ist/fenixedu/cmscomponents/ui/spring/PagesAdminService.java
@@ -149,6 +149,7 @@ public class PagesAdminService {
 
         if (canViewGroup != null && !post.getCanViewGroup().equals(canViewGroup)) {
             post.setCanViewGroup(canViewGroup);
+            menuItem.getChildrenSorted().stream().filter(isStaticPage).forEach(subitem -> postForPage(subitem.getPage()).setCanViewGroup(canViewGroup));
         }
 
         return menuItem;
