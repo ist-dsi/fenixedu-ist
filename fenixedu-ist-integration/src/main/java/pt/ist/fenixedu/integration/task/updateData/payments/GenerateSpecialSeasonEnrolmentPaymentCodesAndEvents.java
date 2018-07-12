1a0cb548e2f4b25a9f11f9ce3317964f83ae24ec
JCAbrantes <joaocarlos.abrantes@gmail.com>
2018-07-12 16:44:27 +0100

    Exclude SAS and students with special needs from GenerateSpecialSeasonEnrolmentPaymentCodesAndEvents

--- fenixedu-ist-integration/src/main/java/pt/ist/fenixedu/integration/task/updateData/payments/GenerateSpecialSeasonEnrolmentPaymentCodesAndEvents.java
+++ fenixedu-ist-integration/src/main/java/pt/ist/fenixedu/integration/task/updateData/payments/GenerateSpecialSeasonEnrolmentPaymentCodesAndEvents.java
@@ -1,7 +1,12 @@
 package pt.ist.fenixedu.integration.task.updateData.payments;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+
 import org.fenixedu.academic.domain.Degree;
 import org.fenixedu.academic.domain.EnrolmentEvaluation;
+import org.fenixedu.academic.domain.ExecutionSemester;
 import org.fenixedu.academic.domain.ExecutionYear;
 import org.fenixedu.academic.domain.Person;
 import org.fenixedu.academic.domain.accounting.PaymentCodeType;
@@ -9,22 +14,42 @@ import org.fenixedu.academic.domain.accounting.events.SpecialSeasonEnrolmentEven
 import org.fenixedu.academic.domain.accounting.paymentCodes.AccountingEventPaymentCode;
 import org.fenixedu.academic.domain.administrativeOffice.AdministrativeOffice;
 import org.fenixedu.academic.domain.student.Registration;
+import org.fenixedu.academic.domain.student.StatuteType;
+import org.fenixedu.academic.domain.student.Student;
 import org.fenixedu.academic.util.EnrolmentEvaluationState;
 import org.fenixedu.academic.util.Money;
+import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.core.groups.Group;
 import org.fenixedu.bennu.scheduler.CronTask;
 import org.fenixedu.bennu.scheduler.annotation.Task;
 import org.fenixedu.messaging.core.domain.Message;
+import org.fenixedu.messaging.core.template.DeclareMessageTemplate;
+import org.fenixedu.messaging.core.template.TemplateParameter;
 import org.joda.time.YearMonthDay;
 
-import java.util.Collections;
+@DeclareMessageTemplate(id = "special.season.enrolment.payment.info.message.template",
+        description = "special.season.enrolment.payment.info.message.description",
+        subject = "special.season.enrolment.payment.info.message.subject",
+        text = "special.season.enrolment.payment.info.message.body",
+        parameters = {
+                @TemplateParameter(id = "eventDescriptionPT",
+                        description = "special.season.enrolment.payment.info.message.parameter.event.description.pt"),
+                @TemplateParameter(id = "eventDescriptionEN",
+                        description = "special.season.enrolment.payment.info.message.parameter.event.description.en"),
+                @TemplateParameter(id = "paymentEntityCode", description = "special.season.enrolment.payment.info.message.parameter.entity.code"),
+                @TemplateParameter(id = "paymentCode", description = "special.season.enrolment.payment.info.message.parameter.code"),
+                @TemplateParameter(id = "amount", description = "special.season.enrolment.payment.info.message.parameter.amount"),
+        },
+        bundle = "resources.FenixeduIstIntegrationResources")
 
 @Task(englishTitle = "Create Events and Payment Codes for Special Season Enrolments", readOnly = false)
 public class GenerateSpecialSeasonEnrolmentPaymentCodesAndEvents extends CronTask {
 
     private static final Money AMOUNT_TO_PAY = new Money("20");
-    private static final String EVENT_DESCRIPTION = "Inscrição em época especial à disciplina ";
+    private static final String EVENT_DESCRIPTION_PT = "Inscrição em época especial à disciplina ";
+    private static final String EVENT_DESCRIPTION_EN = "Special Season Enrolment Course ";
+    private static final Set<StatuteType> EXCEPTION_STATUTE_TYPES = Bennu.getInstance().getSpecialSeasonStatuteExceptionSet();
 
     @Override
     public void runTask() {
@@ -47,6 +72,16 @@ public class GenerateSpecialSeasonEnrolmentPaymentCodesAndEvents extends CronTas
         final Degree degree = registration.getDegree();
         final AdministrativeOffice office = degree.getAdministrativeOffice();
         final Person person = registration.getPerson();
+        final Student student = person.getStudent();
+        final Collection<StatuteType> statuteTypes =
+                student.getStatutesTypesValidOnAnyExecutionSemesterFor(ee.getEnrolment().getExecutionYear());
+        final User user = registration.getPerson().getUser();
+
+        if (statuteTypes.stream().anyMatch(st -> EXCEPTION_STATUTE_TYPES.contains(st))) {
+            taskLog("Skipped payment for student due to exception statute: %s%n", user.getUsername());
+            return;
+        }
+
         final SpecialSeasonEnrolmentEvent event = new SpecialSeasonEnrolmentEvent(office, person, Collections.singleton(ee));
 
         final YearMonthDay today = new YearMonthDay();
@@ -54,47 +89,35 @@ public class GenerateSpecialSeasonEnrolmentPaymentCodesAndEvents extends CronTas
                 AccountingEventPaymentCode.create(PaymentCodeType.SPECIAL_SEASON_ENROLMENT, today, today.plusDays(10), event,
                         AMOUNT_TO_PAY, AMOUNT_TO_PAY, event.getPerson());
 
-        final User user = registration.getPerson().getUser();
-        final String eventDescription = eventDescription(ee);
+        final String eventDescriptionPT = eventDescriptionPT(ee);
+        final String eventDescriptionEN = eventDescriptionEN(ee);
 
-        taskLog("Generated payment codes for: %s -> %s%n", user.getUsername(), eventDescription);
+        taskLog("Generated payment codes for: %s -> %s%n", user.getUsername(), eventDescriptionEN);
 
-        final String subject = "Pagamento Inscrição Época Especial - " + eventDescription;
-        final String body =
-                "A inscrição em exames de época especial ou extraordinário está sujeita ao " +
-                "pagamento de um emolumento de 20 Euros por unidade curricular. " +
-                "\n\n" +
-                "A sua inscrição só é finalizada após o pagamento do referido emolumento " +
-                "usando a referência multibanco junta. " +
-                "\n\n" +
-                "Há uma referência multibanco associada a cada unidade curricular a que " + 
-                "pretende inscrever-se. " +
-                "\n\n" +
-                "Este pagamento deverá ser efectuado no prazo de 48h, sob pena " +
-                "de a inscrição deixar de ser válida. " +
-                "\n\n" +
-                "Pode efetuar o pagamento via multibanco com os seguintes dados a partir " +
-                "das 20:00 do dia de hoje (ou das 20:00 do dia seguinte se esta mensagem " +
-                "tiver sido enviada depois das 18h00): " +
-                "\n\n" +
-                "Entidade: " + paymentCode.getEntityCode() + "\n" +
-                "\n" +
-                "Referência: " + paymentCode.getCode() + "\n\n" +
-                "\n" +
-                "Valor: " + AMOUNT_TO_PAY.toString() + " €" +
-                "\n\n"
-                ;
         Message.fromSystem()
-                .replyToSender()
-                .to(Group.users(user))
-                .subject(subject)
-                .textBody(body)
-                .send();
+               .replyToSender()
+               .to(Group.users(user))
+               .template("special.season.enrolment.payment.info.message.template")
+                   .parameter("eventDescriptionPT", eventDescriptionPT)
+                   .parameter("eventDescriptionEN", eventDescriptionEN)
+                   .parameter("paymentEntityCode", paymentCode.getEntityCode())
+                   .parameter("paymentCode", paymentCode.getCode())
+                   .parameter("amount", AMOUNT_TO_PAY.toString())
+                   .and()
+               .send();
+
     }
 
-    private String eventDescription(final EnrolmentEvaluation ee) {
-        return EVENT_DESCRIPTION + ee.getEnrolment().getCurricularCourse().getName(ee.getExecutionPeriod()) + " - "
+    private String eventDescriptionPT(final EnrolmentEvaluation ee) {
+        return EVENT_DESCRIPTION_PT + ee.getEnrolment().getCurricularCourse().getName(ee.getExecutionPeriod()) + " - "
                 + ee.getExecutionPeriod().getQualifiedName();
     }
 
+    private String eventDescriptionEN(final EnrolmentEvaluation ee) {
+        ExecutionSemester executionSemester = ee.getExecutionPeriod();
+        String semester = executionSemester.getSemester() == 1 ? "1st" : "2nd" + " Semester ";
+        String year = executionSemester.getExecutionYear().getYear().toString();
+        return EVENT_DESCRIPTION_EN + ee.getEnrolment().getCurricularCourse().getNameEn(ee.getExecutionPeriod()) + " - "
+                + semester + year;
+    }
 }
