61e42ba49046b1665a24cde4765d278c023e5ee7
sergiofbsilva <sergiofbsilva@gmail.com>
2019-06-12 19:23:11 +0100

    Fix campus for researchers and employees

--- fenixedu-ist-integration/src/main/java/pt/ist/fenixedu/integration/ui/spring/santander/service/UserInformationService.java
+++ fenixedu-ist-integration/src/main/java/pt/ist/fenixedu/integration/ui/spring/santander/service/UserInformationService.java
@@ -2,19 +2,30 @@ package pt.ist.fenixedu.integration.ui.spring.santander.service;
 
 import java.awt.image.BufferedImage;
 import java.util.List;
+import java.util.function.BiFunction;
 
 import org.fenixedu.academic.domain.Person;
 import org.fenixedu.academic.domain.photograph.Picture;
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.idcards.service.IUserInfoService;
+import org.fenixedu.spaces.domain.Space;
 import org.springframework.stereotype.Service;
 
 import com.google.common.io.BaseEncoding;
-
+import pt.ist.fenixedu.contracts.domain.accessControl.ActiveEmployees;
+import pt.ist.fenixedu.contracts.domain.accessControl.ActiveGrantOwner;
+import pt.ist.fenixedu.contracts.domain.accessControl.CampusEmployeeGroup;
+import pt.ist.fenixedu.contracts.domain.accessControl.CampusGrantOwnerGroup;
 import pt.ist.fenixedu.integration.dto.PersonInformationDTO;
 
 @Service
 public class UserInformationService implements IUserInfoService {
+
+    private final static BiFunction<Space, User, Boolean> employeeGroupFunction =
+            (space, employeeUser) -> CampusEmployeeGroup.get(space).isMember(employeeUser);
+
+    private final static BiFunction<Space, User, Boolean> grantOwnerGroupFunction =
+            (space, employeeUser) -> CampusGrantOwnerGroup.get(space).isMember(employeeUser);
 
     @Override
     public List<String> getUserRoles(User user) {
@@ -45,6 +56,16 @@ public class UserInformationService implements IUserInfoService {
     @Override
     public String getCampus(User user) {
         PersonInformationDTO personInformationDTO = new PersonInformationDTO(user.getPerson());
-        return personInformationDTO.getCampus();
+        String campus = personInformationDTO.getCampus();
+
+        if (campus == null) {
+            final BiFunction<Space, User, Boolean> groupFunction =
+                    new ActiveEmployees().isMember(user) ? employeeGroupFunction : (new ActiveGrantOwner()
+                            .isMember(user) ? grantOwnerGroupFunction : null);
+
+            campus = groupFunction == null ? null : Space.getTopLevelSpaces().stream()
+                    .filter(space -> groupFunction.apply(space, user)).findAny().map(Space::getName).orElse(null);
+        }
+        return campus;
     }
 }
