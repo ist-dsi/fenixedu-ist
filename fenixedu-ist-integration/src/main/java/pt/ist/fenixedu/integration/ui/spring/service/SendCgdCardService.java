a0b3b20f7bd32b5af62518b096d415792084f968
Marco Cerdeira <marco.cerdeira@tecnico.ulisboa.pt>
2021-09-16 12:18:29 +0100

    SendCgdCardService with own thread FIST-930 #resolve

--- fenixedu-ist-integration/src/main/java/pt/ist/fenixedu/integration/ui/spring/service/SendCgdCardService.java
+++ fenixedu-ist-integration/src/main/java/pt/ist/fenixedu/integration/ui/spring/service/SendCgdCardService.java
@@ -23,6 +23,7 @@ import pt.ist.fenixframework.FenixFramework;
 @Service
 public class SendCgdCardService {
     private static final Logger LOGGER = LoggerFactory.getLogger(SendCgdCardService.class);
+    private static final int waitForThread = 3000;
 
     private RegistrationDeclarationForBanksService registrationDeclarationForBanksService;
 
@@ -32,43 +33,71 @@ public class SendCgdCardService {
         this.registrationDeclarationForBanksService = registrationDeclarationForBanksService;
     }
 
-    @Atomic(mode = TxMode.READ)
     public String sendCgdCard(CgdCard card) {
-        if (card == null) {
-            return "CGD: Não existe cartão para este pedido.";
+        try {
+            CgdRunnable cgdRunnable = new CgdRunnable(card);
+            Thread thread = new Thread(cgdRunnable);
+            thread.start();
+            thread.join(waitForThread);
+            return cgdRunnable.getResult();
+        } catch (Exception e) {
+            return "CGD: Problemas de comunicação.";
         }
-        final Person person = card.getUser().getPerson();
-        final String username = card.getUser().getUsername();
-        if (BooleanUtils.isTrue(card.getAllowSendBankDetails())) {
-            if (person != null) {
-                final Student student = person.getStudent();
-                if (student != null) {
-                    for (final Registration registration : student.getRegistrationsSet()) {
-                        if (registration.isActive()) {
-                            CgdForm43Sender sender = new CgdForm43Sender();
-                            boolean form = sender.sendForm43For(registration);
-                            boolean attachment = sender.uploadFormAttachment(registration,registrationDeclarationForBanksService
-                                    .getRegistrationDeclarationFileForBanks(registration));
-                            LOGGER.info("Sent Form43 ({}) and registration declaration file ({}) for registration {}",
-                                    form, attachment, registration.getExternalId() );
-                            if (form && attachment) {
-                                FenixFramework.atomic(() -> card.setSuccessfulSentData(new DateTime()));
-                                return String.format("CGD: Comunicação efectuada à CGD com sucesso para o utilizador %s",
-                                        username);
-                            } else {
-                                return String.format("CGD: Comunicação falhou para o utilizador %s. Contactar a CGD.", username);
-                            }
-                        }
-                    }
-                    return String.format("CGD: Não existe uma matrícula activa para o aluno %s", username);
-                }
-                return String.format("CGD: Utilizador %s não é aluno", username);
-            } else {
-                return  String.format("CGD: Utilizador %s não tem pessoa activa", username);
-            }
-        }
-        return String.format("CGD: %s - É necessário autorização a cedência de dados à CGD para efeitos de abertura de conta",
-                username) ;
     }
 
+    private class CgdRunnable implements Runnable {
+
+        private CgdCard card;
+        private String result;
+        public CgdRunnable(CgdCard card) {
+            this.card = card;
+        }
+
+        @Atomic(mode = TxMode.READ)
+        public void run() {
+            if (this.card == null) {
+                LOGGER.info("CGD: Não existe cartão para este pedido.");
+                return;
+            }
+            final Person person = this.card.getUser().getPerson();
+            final String username = this.card.getUser().getUsername();
+            if (BooleanUtils.isTrue(this.card.getAllowSendBankDetails())) {
+                if (person != null) {
+                    final Student student = person.getStudent();
+                    if (student != null) {
+                        for (final Registration registration : student.getRegistrationsSet()) {
+                            if (registration.isActive()) {
+                                CgdForm43Sender sender = new CgdForm43Sender();
+                                boolean form = sender.sendForm43For(registration);
+                                boolean attachment = sender.uploadFormAttachment(registration,registrationDeclarationForBanksService
+                                        .getRegistrationDeclarationFileForBanks(registration));
+                                LOGGER.info("Sent Form43 ({}) and registration declaration file ({}) for registration {}",
+                                        form, attachment, registration.getExternalId() );
+                                if (form && attachment) {
+                                    FenixFramework.atomic(() -> card.setSuccessfulSentData(new DateTime()));
+                                    result = String.format("CGD: Comunicação efectuada à CGD com sucesso para o utilizador %s", username);
+                                    return;
+                                } else {
+                                    result = String.format("CGD: Comunicação falhou para o utilizador %s. Contactar a CGD.", username);
+                                    return;
+                                }
+                            }
+                        }
+                        result = String.format("CGD: Não existe uma matrícula activa para o aluno %s", username);
+                        return;
+                    }
+                    result = String.format("CGD: Utilizador %s não é aluno", username);
+                    return;
+                } else {
+                    result = String.format("CGD: Utilizador %s não tem pessoa activa", username);
+                    return;
+                }
+            }
+            result = String.format("CGD: %s - É necessário autorização a cedência de dados à CGD para efeitos de abertura de conta", username);
+        }
+
+        public String getResult() {
+            return this.result;
+        }
+    }
 }
